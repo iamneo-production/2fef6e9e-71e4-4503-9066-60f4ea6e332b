0c699d62baf89ba482c670a197fa32bcea082bc3
sakshipenurkar <sakshipenurkar@gmail.com>
2023-08-04 11:30:19 +0000

    healthcare

---
+++ healthcareserver/src/test/java/com/healthcare/healthcareserver/HealthcareserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthcare.healthcareserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthcareserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ registrationservice/src/main/java/com/healthcare/registration/registrationservice/controller/PatientRepo.java
@@ -0,0 +1,6 @@
+package com.healthcare.registration.registrationservice.controller;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PatientRepo extends JpaRepository<Patient,Long> {
+    
+}
